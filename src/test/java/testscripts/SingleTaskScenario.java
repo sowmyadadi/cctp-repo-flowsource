033f8e1ea6a1ac4fe79a9c4bb47664efe97baa9f
sowmyadadi <150822093+sowmyadadi@users.noreply.github.com>
2026-08-18 17:23:51 +0530

    Change data provider for Todo List test

--- src/test/java/testscripts/SingleTaskScenario.java
+++ src/test/java/testscripts/SingleTaskScenario.java
@@ -8,7 +8,7 @@ import com.cognizant.framework.selenium.SeleniumTestParameters;
 
 public class SingleTaskScenario extends TestConfigurations {
 
-	@Test(dataProvider = "ChromeBrowser", dataProviderClass = TestConfigurations.class)
+	@Test(dataProvider = "ChromeHeadlessBrowser", dataProviderClass = TestConfigurations.class)
 	public void TestForCreatingTodoList(SeleniumTestParameters testParameters) {
 
 		testParameters.setCurrentTestDescription("Test for creating a Todo List");
